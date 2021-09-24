865b3b9e20eb3529c449f503cc7920cbec6f029b
Alejandro-17 <diego.osorio2@udea.edu.co>
2021-09-24 14:03:11 -0500

    Avance hasta la creacion del escenario principal en proyecto nuevo

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "papabom.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Papabom w;
+    w.show();
+    return a.exec();
+}
---
+++ papabom.h
@@ -0,0 +1,29 @@
+#ifndef PAPABOM_H
+#define PAPABOM_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QImage>
+#include <vector>
+
+#define Ruta_imagen "../Papabom/imagenes/gris.jpg"
+
+using namespace std;
+QT_BEGIN_NAMESPACE
+namespace Ui { class Papabom; }
+QT_END_NAMESPACE
+
+class Papabom : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Papabom(QWidget *parent = nullptr);
+    ~Papabom();
+
+private:
+    Ui::Papabom *ui;
+    QGraphicsScene *scene;
+    vector <QGraphicsRectItem *> bloques;
+};
+#endif // PAPABOM_H
